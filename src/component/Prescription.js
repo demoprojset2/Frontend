92f66dc796ecb8a40c613e3b6dc8cbebee0f00f1
sparsh1303 <sparshgoyal9@gmail.com>
2022-01-14 23:46:27 +0530

    Prescription file

--- src/component/Prescription.js
+++ src/component/Prescription.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Dasboard.css";
-const PatientDashboard = () => {
+import axios from "axios";
+class Prescription extends Component {
+	constructor(props) {
+        super(props);
+   
+        this.state = {
+            items: [],
+            DataisLoaded: false
+        };
+    }
+	componentDidMount() {
+        fetch(
+"https://jsonplaceholder.typicode.com/users")
+            .then((res) => res.json())
+            .then((json) => {
+                this.setState({
+                    items: json,
+                    DataisLoaded: true
+                });
+            })
+    }
+
+
+		render() {
     return (
       <div>
-<div class="col-md-6 card-container-container ">
+{/* <div class="col-md-6 card-container-container "> */}
 				<div class="card text-center">
 					<div class="card-header">
 						<h3>Patient's Detail</h3>
@@ -17,10 +40,22 @@ const PatientDashboard = () => {
                         <p class="card-text">asdfghjknbv </p>
                         <p class="card-text">asdfghjknbv </p>
 					</div>
+					{/* <div>
+					{
+                items.map((item) => ( 
+                <ol key = { item.id } >
+                    User_Name: { item.username }, 
+                    Full_Name: { item.name }, 
+                    User_Email: { item.email } 
+                    </ol>
+                ))
+            }
+			</div> */}
 				</div>
-		    </div>
+		    {/* </div> */}
 </div>
   );
-};
+}
+	}
 
-export default PatientDashboard;
+export default Prescription;
