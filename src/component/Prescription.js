58ae2cc1a4e6fbee249d09d5698e2af3ba35b1e2
sparsh1303 <sparshgoyal9@gmail.com>
2022-01-14 21:15:38 +0530

    Prescription form by Sparsh

---
+++ src/component/Prescription.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Dasboard.css";
+const PatientDashboard = () => {
+    return (
+      <div>
+<div class="col-md-6 card-container-container ">
+				<div class="card text-center">
+					<div class="card-header">
+						<h3>Patient's Detail</h3>
+					</div>
+					<div class="card-body preamble" id="zen-preamble" role="article">
+						<p class="card-text"> Name : </p>
+						<p class="card-text"> Date : </p>
+						<p class="card-text">sdfghj</p>
+					    <p class="card-text">asdfghjknbv </p>
+                        <p class="card-text">asdfghjknbv </p>
+                        <p class="card-text">asdfghjknbv </p>
+                        <p class="card-text">asdfghjknbv </p>
+					</div>
+				</div>
+		    </div>
+</div>
+  );
+};
+
+export default PatientDashboard;
