fbba20389998c83da688e87f6214280a1493aa57
sparsh1303 <sparshgoyal9@gmail.com>
2022-01-16 12:01:44 +0530

    UPdated

--- src/component/Medical.js
+++ src/component/Medical.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 // import "./Dasboard.css";
-import "./Prescription.css";
-// import "./View.css";
+import "./Medical.css";
+
 import axios from "axios";
 class Prescription extends Component {
 	constructor(props) {
@@ -9,20 +9,32 @@ class Prescription extends Component {
    
         this.state = {
             items: [],
+            doses:[],
             DataisLoaded: false
         };
+        let patient_id=localStorage.getItem('patient_id')
     }
-// 	componentDidMount() {
-//         fetch(
-// "https://jsonplaceholder.typicode.com/users")
-//             .then((res) => res.json())
-//             .then((json) => {
-//                 this.setState({
-//                     items: json,
-//                     DataisLoaded: true
-//                 });
-//             })
-//     }
+	componentDidMount() {
+        fetch(
+"http://127.0.0.1:8000/api/patient/${patient_id}/medications")
+            .then((res) => res.json())
+            .then((json) => {
+                this.setState({
+                    items: json,
+                    DataisLoaded: true
+                });
+            })
+
+            fetch(
+                "http://127.0.0.1:8000/api/patient/${patient_id}/dose")
+                            .then((res) => res.json())
+                            .then((json) => {
+                                this.setState({
+                                    doses: json,
+                                    
+                                });
+                            })
+    }
 
 
 		render() {
@@ -34,45 +46,50 @@ class Prescription extends Component {
             <div class="col-lg-8 grid-margin stretch-card">
                 <div class="card">
                     <div class="card-body">
-                        <h4 class="card-title">Prescription Details</h4>
-                        <p class="card-description"> Basic table with card </p>
+                        <h4 class="card-title">Medical Details</h4>
+                        {/* <p class="card-description">Prescription  </p> */}
                         <div class="table-responsive">
                             <table class="table">
-                                <thead>
-                                    <tr>
-                                        <th>Name</th>
-                                        
-                                        <th>Created On</th>
-                                        <th>Status</th>
-                                    </tr>
-                                </thead>
+                               
                                 <tbody>
                                     <tr>
-                                        <td>Samso Park</td>
+                                        <td>Medication_name</td>
                                         
                                         <td>12 May 2017</td>
                                         <td><label class="badge badge-danger">Pending</label></td>
                                     </tr>
                                     <tr>
-                                        <td>Marlo Sanki</td>
+                                        <td>Medication_manufacturer</td>
                                        
                                         <td>15 May 2015</td>
                                         <td><label class="badge badge-warning">In progress</label></td>
                                     </tr>
                                     <tr>
-                                        <td>John ryte</td>
+                                        <td>Expire</td>
                                        
                                         <td>14 May 2017</td>
                                         <td><label class="badge badge-info">Fixed</label></td>
                                     </tr>
                                     <tr>
-                                        <td>Peter mark</td>
+                                        <td>Amount</td>
                                        
                                         <td>16 May 2017</td>
                                         <td><label class="badge badge-success">Completed</label></td>
                                     </tr>
                                     <tr>
-                                        <td>Dave</td>
+                                        <td>Dose_amount</td>
+                                        
+                                        <td>20 May 2017</td>
+                                        <td><label class="badge badge-warning">In progress</label></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Dose_timing</td>
+                                        
+                                        <td>20 May 2017</td>
+                                        <td><label class="badge badge-warning">In progress</label></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Dose_description</td>
                                         
                                         <td>20 May 2017</td>
                                         <td><label class="badge badge-warning">In progress</label></td>
