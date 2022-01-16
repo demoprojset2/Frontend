1001dcc6aa182db16872212d2a6451a9be6dd9bb
demoprojset2 <97522797+demoprojset2@users.noreply.github.com>
2022-01-17 00:36:28 +0530

    Routes updated by Syed Mohd Taha

--- src/App.js
+++ src/App.js
@@ -8,10 +8,17 @@ import Navbar from './component/Navbar';
 import SignUpForm from "./component/SignUpForm";
 import SignInForm from "./component/SignInForm";
 import PatientLogin from "./component/Patientlogin";
-import Doctor from "./component/Doctor";
+import Doctor from "./component/AddPatient";
+import Modals from "./component/Modals";
+import PatientDashboard2 from "./component/PatientDashboard2";
+import Me2 from "./component/Me2";
 
 import Body from "./component/Body";
 import Doctordashboard from "./component/Doctor-Dashboard";
+import ViewPatient from "./component/ViewPatients";
+import PatientDashboard from './component/PatientDashboard'
+import ProblemDetails from "./component/patient/ProblemDetails";
+import Prescriptions from "./component/patient/Prescriptions";
 // import Home from "./component/Home"
 
 // function App() {
@@ -44,15 +51,26 @@ class App extends Component {
             
             
             <Routes>
-              <Route exact path="/" element={<Home />} ></Route>
+              <Route path="/" element={<Home />} ></Route>
               {/* <div className="App"> */}
               <Route path="/sign-up" element={<SignUpForm />} ></Route>
               <Route path="/sign-in" element={<SignInForm />} />
-              <Route path="/patient-login" element={<PatientLogin />} />
-              <Route path="/doctor" element={<Doctor />} />
+              <Route path="/addpatient" element={<Doctor />} />
+              <Route path="/modals" element={<Modals />} />
+
         
               <Route exact path="/body" element={<Body />} ></Route>
             <Route  path="/docdash" element={<Doctordashboard />} ></Route>
+            <Route  path="/patientlist" element={<ViewPatient />} ></Route>
+
+            <Route path="/patient-login" element={<PatientLogin />} />
+              <Route path="/patient-dashboard/:id" element={<PatientDashboard />} />
+
+              <Route path="/problem-details" element={<ProblemDetails />} />
+              <Route path="/prescriptions" element={<Prescriptions />} />
+              <Route  path="/me" element={<Me2/>} ></Route>
+              <Route  path="/viewdetails" element={<PatientDashboard2/>} ></Route>
+
 
               {/* </div> */}
               
