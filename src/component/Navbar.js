fbb6d2e6fddd4c22b31db88b1890ecaea4996633
sparsh1303 <sparshgoyal9@gmail.com>
2022-01-17 00:30:24 +0530

    Navbar

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,8 +1,19 @@
+import { Navigate } from "react-router-dom";
+import {useEffect } from "react";
+
+
+const logout = () => {
+  localStorage.clear();
+  window.location.href = "/sign-in"
+
+}
 const Navbar = ({title}) => {
+
+ 
   return (
     <div >
-      <nav className="navbar navbar-expand-lg navbar-light bg-light" >
-        <a className="navbar-brand" href="#">
+      <nav className="navbar navbar-expand-lg navbar-light bg-dark" >
+        <a className="navbar-brand text-white" href="/docdash">
           Healthcare
         </a>
         <button
@@ -18,20 +29,13 @@ const Navbar = ({title}) => {
         </button>
         <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
           <ul className="navbar-nav">
-            <li className="nav-item active">
-              <a className="nav-link" href="#">
-                Home <span className="sr-only">(current)</span>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                About
-              </a>
+          
+            <li  className="nav-item ">
+              <button onClick={logout} className="nav-link btn-danger text-white rounded" >
+                log out
+              </button>
             </li>
             
-            <li className="nav-item">
-              <a className="nav-link" href="#">Contact</a>
-            </li>
           </ul>
         </div>
       </nav>
