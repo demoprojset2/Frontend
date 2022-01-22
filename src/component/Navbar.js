2cb30b2dca683b9082e61da86cf6b03150cbd159
syedmohdtaha-cs <62473304+syedmohdtaha-cs@users.noreply.github.com>
2022-01-22 12:05:58 +0530

    Adding patient add functionality

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,9 +1,27 @@
-const Navbar = ({title}) => {
+import { Link, Navigate } from "react-router-dom";
+import { useEffect } from "react";
+
+const logout = () => {
+  localStorage.clear();
+  window.location.href = "/sign-in";
+};
+const Navbar = ({ title }) => {
+
+  // useEffect(() => {
+
+  //   const token = localStorage.getItem("token");
+
+  //   if(token == null){
+  //     return <Navigate to="/sign-in" />
+  //   }
+
+  // })
+
   return (
-    <div >
-      <nav className="navbar navbar-expand-lg navbar-light bg-light" >
-        <a className="navbar-brand" href="#">
-          Healthcare
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a className="navbar-brand text-white" href="/docdash">
+          <h2>Online eHR</h2>
         </a>
         <button
           className="navbar-toggler"
@@ -14,24 +32,39 @@ const Navbar = ({title}) => {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span className="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon "></span>
         </button>
-        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <div
+          className="collapse navbar-collapse justify-content-end"
+          id="navbarNav"
+        >
           <ul className="navbar-nav">
-            <li className="nav-item active">
-              <a className="nav-link" href="#">
-                Home <span className="sr-only">(current)</span>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                About
-              </a>
-            </li>
+          <li className="nav-item ">
+          
+              <Link
+                to="/addpatient"
+                style={{ fontSize: "20px" ,marginTop:"2px"}}
+              
+                className="nav-link btn-outline-info border border-info btn-lg text-white rounded font-weight-bold mr-3 pt-2"
+              >
+                Add Patient
+              </Link>
             
-            <li className="nav-item">
-              <a className="nav-link" href="#">Contact</a>
             </li>
+            <li className="nav-item ">
+              <button
+                style={{ fontSize: "20px" }}
+                onClick={logout}
+                className="nav-link btn-danger text-white rounded font-weight-bold"
+              >
+                Logout
+              </button>
+            </li>
+            {/* <li className="nav-item">
+              <a className="nav-link" href="/addpatient">
+                Add Patient
+              </a>
+            </li> */}
           </ul>
         </div>
       </nav>
